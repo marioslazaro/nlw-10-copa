3d9c62c5d594d9a0340e3ddaf0b9f805f2993763
marioslazaro <mario-msl@hotmail.com>
2022-11-11 20:19:58 -0300

    :tadda: inittial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+function createGame(player1, hour, player2) {
+  return `
+   <li>
+       <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira de ${player2}">
+     </li>
+  `
+}
+
+function createCard(date, day, games) {
+  return ` 
+         <div class="card">
+            <h2>${date}<span>${day}</span></h2>
+            <ul>
+             ${games}
+            </ul>
+        </div>
+  
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("south korea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "serbia"))
